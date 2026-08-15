a7b0bd15bf959eddced02e001aa2063e5eddf1cc
Trooper C6-P7 <trooperc6p7@gmail.com>
2026-08-15 00:26:43 -0400

    Update AutoManifest.py

--- AutoManifest/AutoManifest.py
+++ AutoManifest/AutoManifest.py
@@ -20,6 +20,7 @@ class NodeOption:
     Name : str = ""
     Description : str = ""
     Image : Path = field(default_factory = lambda: Path("~"))
+    Include : list[str] = field(default_factory = lambda: [])
     SubOptions : list[LeafOption] = field(default_factory = lambda: [])
 
 #class for the manifest
@@ -42,6 +43,7 @@ def main():
         with open(f"{main_dir}/manifest.json", "r", encoding="utf-8") as prev_file:
             prev_manifest = json.load(prev_file)
         with open(f"{main_dir}/old_manifests.txt", "a") as store_file:
+            store_file.write("\n\n\nMANIFEST\n\n\n")
             store_file.write(json.dumps(prev_manifest, indent="\t"))
     else:
         prev_manifest = None
@@ -60,80 +62,89 @@ def main():
     images = sorted(main_dir.rglob(f"{main_dir.stem}.[jp][pn]g"))
     if images: manifest.IconPath = f"""{images[0].relative_to(main_dir).as_posix()}"""
 
-    #loop through option folders
-    for mod_sub_dir in main_dir.iterdir():
+    #loop through organization folders
+    for org_dir in sorted(main_dir.iterdir()):
         #make sure it's a folder
-        if mod_sub_dir.is_dir():
-            #check if option folder has sub-option folders
-            if any(item.is_dir() for item in mod_sub_dir.iterdir()):
-                #create new option
-                option = NodeOption()
+        if org_dir.is_dir():
+            #loop through option folders
+            for mod_sub_dir in sorted(org_dir.iterdir()):
+                #make sure it's a folder
+                if mod_sub_dir.is_dir():
+                    #check if option folder has sub-option folders
+                    if any(item.is_dir() for item in mod_sub_dir.iterdir()):
+                        #create new option
+                        option = NodeOption()
+                    
+                        #find and add option image
+                        images = sorted(main_dir.rglob(f"{mod_sub_dir.stem}.[jp][pn]g"))
+                        if images: option.Image = f"""{images[0].relative_to(main_dir).as_posix()}"""
 
-                #loop through sub-option folders
-                for mod_subsub_dir in mod_sub_dir.iterdir():
-                    #make sure it's a folder
-                    if mod_subsub_dir.is_dir():
-                        #exit if sub-option folder has internal folders (improper layout)
-                        if any(item.is_dir() for item in mod_subsub_dir.iterdir()):
-                            input("Folders nested incorrectly! Press any key to exit...")
-                            sys.exit()
-                        
-                        #skip if sub-option folder doesn't have patch files
-                        elif not any(".patch_" in str(item) for item in mod_subsub_dir.iterdir()):
-                            continue
+                        #loop through sub-option folders
+                        for mod_subsub_dir in sorted(mod_sub_dir.iterdir()):
+                            #make sure it's a folder
+                            if mod_subsub_dir.is_dir():
+                                #exit if sub-option folder has internal folders (improper layout)
+                                if any(item.is_dir() for item in mod_subsub_dir.iterdir()):
+                                    input(f"Invalid folder nesting: {mod_subsub_dir}! Press any key to exit...")
+                                    sys.exit()
+                                
+                                #skip if sub-option folder doesn't have patch files
+                                elif not any(".patch_" in str(item.name) for item in mod_subsub_dir.iterdir()):
+                                    continue
 
-                        #create new sub-option
-                        sub_option = LeafOption()
-                        sub_option.Name = mod_subsub_dir.name
-                        sub_option.Include = [f"{mod_sub_dir.name}/{mod_subsub_dir.name}"]
-                        
-                        #use pre-existing sub-option description
-                        if prev_manifest != None:
-                            sub_option.Description = next((
-                                sub_item.get("Description") 
-                                for item in prev_manifest.get("Options", []) 
-                                for sub_item in item.get("SubOptions", [])
-                                if sub_item.get("Name") == mod_subsub_dir.name), "")
+                                #create new sub-option
+                                sub_option = LeafOption()
+                                sub_option.Name = mod_subsub_dir.name
+                                sub_option.Include = [f"{org_dir.name}/{mod_sub_dir.name}/{mod_subsub_dir.name}"]
+                                
+                                #use pre-existing sub-option description
+                                if prev_manifest != None:
+                                    sub_option.Description = next((
+                                        sub_item.get("Description") 
+                                        for item in prev_manifest.get("Options", []) 
+                                        for sub_item in item.get("SubOptions", [])
+                                        if item.get("Name") == mod_sub_dir.name
+                                        and sub_item.get("Name") == mod_subsub_dir.name), "")
 
-                        #find and add sub-option image
-                        images = sorted(main_dir.rglob(f"{mod_subsub_dir.stem}.[jp][pn]g"))
-                        if images: sub_option.Image = f"""{images[0].relative_to(main_dir).as_posix()}"""
+                                #find and add sub-option image
+                                images = sorted(main_dir.rglob(f"{mod_sub_dir.stem} {mod_subsub_dir.stem}.[jp][pn]g"))
+                                if images: sub_option.Image = f"""{images[0].relative_to(main_dir).as_posix()}"""
+                                else:
+                                    images = sorted(mod_subsub_dir.glob("*.[jp][pn]g"))
+                                    if images: sub_option.Image = f"""{images[0].relative_to(main_dir).as_posix()}"""
+
+                                #add sub-option to option
+                                option.SubOptions.append(sub_option)
+
+                    #check if option folder has patch files
+                    elif any(".patch_" in str(item.name) for item in mod_sub_dir.iterdir()):
+                        #create new option
+                        option = LeafOption()
+                    
+                        #find and add option image
+                        images = sorted(main_dir.rglob(f"{mod_sub_dir.stem}.[jp][pn]g"))
+                        if images: option.Image = f"""{images[0].relative_to(main_dir).as_posix()}"""
                         else:
-                            images = sorted(mod_subsub_dir.glob("*.[jp][pn]g"))
-                            if images: sub_option.Image = f"""{images[0].relative_to(main_dir).as_posix()}"""
+                            images = sorted(mod_sub_dir.glob("*.[jp][pn]g"))
+                            if images: option.Image = f"""{images[0].relative_to(main_dir).as_posix()}"""
 
-                        #add sub-option to option
-                        option.SubOptions.append(sub_option)
+                    #skip if option folder doesn't have sub-option folders or patch files
+                    else:
+                        continue
 
-            #check if option folder has patch files
-            elif any(".patch_" in str(item) for item in mod_sub_dir.iterdir()):
-                #create new option
-                option = LeafOption()
-                option.Include = [f"{mod_sub_dir.name}"]
-
-            #skip if option folder doesn't have sub-option folders or patch files
-            else:
-                continue
-
-            #finish creating new option
-            option.Name = mod_sub_dir.name
-            
-            #use pre-existing option description
-            if prev_manifest != None:
-                option.Description = next((
-                    item.get("Description") 
-                    for item in prev_manifest.get("Options", []) 
-                    if item.get("Name") == mod_sub_dir.name), "")
-            
-            #find and add option image
-            images = sorted(main_dir.rglob(f"{mod_sub_dir.stem}.[jp][pn]g"))
-            if images: option.Image = f"""{images[0].relative_to(main_dir).as_posix()}"""
-            else:
-                images = sorted(mod_sub_dir.glob("*.[jp][pn]g"))
-                if images: option.Image = f"""{images[0].relative_to(main_dir).as_posix()}"""
-            
-            #add option to manifest
-            manifest.Options.append(option)
+                    #finish creating new option
+                    option.Name = mod_sub_dir.name
+                    option.Include = [f"{org_dir.name}/{mod_sub_dir.name}"]
+                    
+                    #use pre-existing option description
+                    if prev_manifest != None:
+                        option.Description = next((
+                            item.get("Description") 
+                            for item in prev_manifest.get("Options", []) 
+                            if item.get("Name") == mod_sub_dir.name), "")
+                    
+                    #add option to manifest
+                    manifest.Options.append(option)
 
     #write manifest to manifest.json file
     with open(f"{main_dir}/manifest.json", "w") as main_file:
