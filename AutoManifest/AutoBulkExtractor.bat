feab8d6d367b3827372ddaa209f5a9574f03d29d
Trooper C6-P7 <trooperc6p7@gmail.com>
2026-08-15 00:21:14 -0400

    Delete AutoManifest/AutoBulkExtractor.bat

--- AutoManifest/AutoBulkExtractor.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set "MainDir=Extracted_Files"
-
-if not exist "%MainDir%" mkdir "%MainDir%"
-
-for %%a in (*.zip) do (
-    powershell -command "Expand-Archive -Path '%%a' -DestinationPath '%MainDir%\%%~na' -Force"
-)
-pause
